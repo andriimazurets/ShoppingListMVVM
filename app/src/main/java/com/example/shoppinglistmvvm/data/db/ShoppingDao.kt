f65bd4e83c513641aea04b95f56930fdb9d88915
theri <therigasmazurets@gmail.com>
2023-12-02 18:28:35 +0200

    up

--- app/src/main/java/com/example/shoppinglistmvvm/data/db/ShoppingDao.kt
+++ app/src/main/java/com/example/shoppinglistmvvm/data/db/ShoppingDao.kt
@@ -1,4 +1,4 @@
-package com.example.shoppinglistmvvm
+package com.example.shoppinglistmvvm.data.db
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
@@ -6,11 +6,13 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.example.shoppinglistmvvm.data.db.entities.ShoppingItem
 
 @Dao
 interface ShoppingDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(item: ShoppingItem)
+
     @Delete
     suspend fun delete(item: ShoppingItem)
 
--- app/src/main/java/com/example/shoppinglistmvvm/data/db/ShoppingDatabase.kt
+++ app/src/main/java/com/example/shoppinglistmvvm/data/db/ShoppingDatabase.kt
@@ -1,20 +1,21 @@
-package com.example.shoppinglistmvvm
+package com.example.shoppinglistmvvm.data.db
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.shoppinglistmvvm.data.db.entities.ShoppingItem
 
 @Database(
     entities = [ShoppingItem::class],
     version = 1
 )
-abstract class ShoppingDatabase: RoomDatabase() {
+abstract class ShoppingDatabase : RoomDatabase() {
     abstract fun getShoppingDao(): ShoppingDao
 
     companion object {
         @Volatile
-        private var instance : ShoppingDatabase? = null
+        private var instance: ShoppingDatabase? = null
 
         private val LOCK = Any()
         operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
@@ -22,7 +23,9 @@ abstract class ShoppingDatabase: RoomDatabase() {
         }
 
         private fun createDatabase(context: Context) =
-            Room.databaseBuilder(context.applicationContext,
-                ShoppingDatabase::class.java , "ShoppingDB.db").build()
+            Room.databaseBuilder(
+                context.applicationContext,
+                ShoppingDatabase::class.java, "ShoppingDB.db"
+            ).build()
     }
 }
